26209f4404c565f10fa541cb1a3bc9c5b324164d
Villiam Rose Natividad <villiamrose@gmail.com>
2023-08-10 17:56:57 +0800

    Complete Separation of Concerns 3 exercise

--- js-best-practices/productInventory.js
+++ js-best-practices/productInventory.js
@@ -1,3 +1,26 @@
+const balanceManager = require('./balanceManager');
+
+const products = [
+  {
+    name: 'Skittles',
+    price: 85,
+    id: 'A1',
+  },
+];
+
 module.exports = {
-  
+  getProducts() {
+    return products;
+  },
+
+  getProduct(productId) {
+    const product = products.find((p) => { return p.id === productId; });
+    return product;
+  },
+
+  vendProduct(productId) {
+    const product = this.getProduct(productId);
+    balanceManager.decreaseBalance(product.price);
+    return product;
+  },
 };
--- js-best-practices/vendingMachine.js
+++ js-best-practices/vendingMachine.js
@@ -2,24 +2,7 @@ const balanceManager = require('./balanceManager');
 const changeHandler = require('./changeHandler');
 const productInventory = require('./productInventory');
 
-const products = [
-  {
-    name: 'Skittles',
-    price: 85,
-    id: 'A1',
-  },
-];
-
 module.exports = {
-  getProducts() {
-    return products;
-  },
-
-  getProduct(productId) {
-    const product = products.find((p) => { return p.id === productId; });
-    return product;
-  },
-
   insertCoin(coinType) {
     const value = this.getAmount(coinType);
     balanceManager.increaseBalance(value);
@@ -36,12 +19,6 @@ module.exports = {
     const currentBalance = balanceManager.getBalance();
     balanceManager.decreaseBalance(currentBalance);
     return this.convertToChange(currentBalance);
-  },
-
-  vendProduct(productId) {
-    const product = this.getProduct(productId);
-    balanceManager.decreaseBalance(product.price);
-    return product;
   },
 
 };
