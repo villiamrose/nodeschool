f188d5ce5b4990c0fb67900b3700cdb9ba661f9f
Villiam Rose Natividad <villiamrose@gmail.com>
2023-08-10 17:57:35 +0800

    Complete Getting Rid of Switch Statements exercise

--- js-best-practices/changeHandler.js
+++ js-best-practices/changeHandler.js
@@ -1,16 +1,34 @@
+// COINS:
+// [n]ickel
+// [d]ime
+// [q]uarter
+const coins = [
+  {
+    code: 'p',
+    title: 'penny',
+    value: 1,
+  },
+  {
+    code: 'n',
+    title: 'nickel',
+    value: 5,
+  },
+  {
+    code: 'd',
+    title: 'dime',
+    value: 10,
+  },
+  {
+    code: 'q',
+    title: 'quarter',
+    value: 25,
+  },
+];
+
 module.exports = {
   getAmount(coinType) {
-    // COINS:
-    // [p]enny
-    // [n]ickel
-    // [d]ime
-    // [q]uarter
-    switch (coinType) {
-      case 'p': return 1;
-      case 'n': return 5;
-      case 'd': return 10;
-      case 'q': return 25;
-      default: throw new Error(`Unrecognized coin ${coinType}`);
-    }
+    const coin = coins.find((coin) => coin.code === coinType);
+    if (!coin) throw new Error(`Unrecognized coin ${coinType}`);
+    return coin.value;
   },
 };
